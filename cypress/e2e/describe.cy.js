f9615fb09a3247e1e095fe6d336a35fae43f74d4
manoj-chandran <developermanoj2003@gmail.com>
2024-01-27 09:38:18 +0530

    describe

---
+++ cypress/e2e/describe.cy.js
@@ -0,0 +1,24 @@
+describe.only('Focused Test Suite', () => {
+    it('Test Case 1', () => {
+      cy.log("------ Describe Only 1------")
+    });
+  
+    it('Test Case 2', () => {
+      cy.log("-------- Describe Only 2 -------")
+    });
+  });
+  
+  describe('Another Test Suite', () => {
+    it('Another Test Case', () => {
+      cy.log("------ Describe only Second Block -------")
+    });
+  });
+
+
+
+
+  /* Describe Only runs the specific code to test
+  that is inside of its block, but does not execute any other tests
+  
+  */
+  
---
+++ cypress/e2e/mocha.cy.js
@@ -0,0 +1,23 @@
+describe('Mocha Test', ()=>{
+    it('Mocha Test 1', ()=>{
+
+        cy.log('------ Mocha Test 1 --------');
+    })
+
+    it('Mocha Test 2', ()=>{
+
+        cy.log('------ Mocha Test 2 --------');
+    })
+})
+
+describe('Mocha Test 2', ()=>{
+    it('Mocha Test2 1', ()=>{
+
+        cy.log('------ Mocha Test 1 --------');
+    })
+
+    it('Mocha Test2 2', ()=>{
+
+        cy.log('------ Mocha Test 2 --------');
+    })
+})
