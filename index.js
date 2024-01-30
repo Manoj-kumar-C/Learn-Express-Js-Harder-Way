bd0079e116a81f39d6157993c2e087fcc6d2b7ee
manoj-chandran <developermanoj2003@gmail.com>
2024-01-30 20:17:43 +0530

    Kubernetes

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express")
 
 const app = express();
 
+// importing the swagger ui libraries 
+
+const swagger = require('swagger-ui-express')
+
+const swaggerJsdoc = require('swagger-jsdoc')
+
+
+
 app.set("view engine", "ejs")
 
 app.use(express.json())
@@ -23,5 +31,25 @@ app.get('/register', function(req, res){
 
 app.use("/home", home);
 
+
+
+// swagger implementation 
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: 'Your API',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./src/routes/*.js'], // Specify the path to your route files
+  };
+
+  
+const specs = swaggerJsdoc(swaggerOptions);
+
+// Serve Swagger UI at /api-docs
+app.use('/api-docs', swagger.serve, swagger.setup(specs));
+
 const port = process.env.port || 3000
-app.listen(port, console.log("sucessfully"))
+app.listen(port, console.log("successfully"))
