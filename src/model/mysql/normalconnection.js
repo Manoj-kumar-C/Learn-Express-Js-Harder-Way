fa9dbc678f94ce63146938461b7014ae0f478636
manoj-chandran <developermanoj2003@gmail.com>
2024-01-31 21:19:49 +0530

    planetscale

--- src/model/mysql/normalconnection.js
+++ src/model/mysql/normalconnection.js
@@ -1,28 +1,6 @@
-const mysql = require('mysql2');
-
-// Create a connection to the MySQL database
-const connection = mysql.createConnection({
-  host: 'sql6.freemysqlhosting.net',
-  user: 'sql6680402',
-  password: 'JtAUjkLa86',
-  database: 'sql6680402',
-  port: 3306
-});
-
-// Connect to the database 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-
-  // Close the connection after connecting (if you want)
-  connection.end((err) => {
-    if (err) {
-      console.error('Error closing connection:', err);
-      return;
-    }
-    console.log('Connection closed');
-  });
-});
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+connection.end()
+  
