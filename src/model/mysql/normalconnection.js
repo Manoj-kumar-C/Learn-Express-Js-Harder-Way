82a0d091e09dd90c79daf7128fc8f6b5e9074e18
manoj-chandran <developermanoj2003@gmail.com>
2024-01-31 21:29:30 +0530

    planetscale

--- src/model/mysql/normalconnection.js
+++ src/model/mysql/normalconnection.js
@@ -1,6 +1,15 @@
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('Connected to PlanetScale!')
-connection.end()
-  
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.query('SHOW TABLES', function (error, results, fields) {
+    if (error) throw error;
+
+    console.log('Tables in the database:');
+    results.forEach((result) => {
+        console.log(result[fields[0].name]);
+    });
+
+    connection.end();
+});
