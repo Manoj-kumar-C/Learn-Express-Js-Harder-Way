80d58bf6f95d55f7c1334db7cabf38d299074972
manoj-chandran <developermanoj2003@gmail.com>
2024-02-07 12:24:00 +0530

    added sql railway

--- src/model/mysql/normalconnection.js
+++ src/model/mysql/normalconnection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
-  host: 'sql6.freemysqlhosting.net',
-  user: 'sql6680402',
-  password: 'JtAUjkLa86',
-  database: 'sql6680402',
-  port: 3306
+  // host: 'viaduct.proxy.rlwy.net',
+  // user: 'root',
+  // password: 'dH246eCDHDcDga2A42G633hdghED4aad',
+  // database: 'railway',
+  // port: 56991
 });
 
 // Connect to the database 
@@ -17,25 +17,42 @@ connection.connect((err) => {
   }
   console.log('Connected to MySQL database');
 
-  // Show tables
-  connection.query('SHOW TABLES', (err, results, fields) => {
+  // Create a table
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE
+    )
+  `;
+
+  connection.query(createTableQuery, (err, results, fields) => {
     if (err) {
-      console.error('Error querying for tables:', err);
+      console.error('Error creating table:', err);
       return;
     }
+    console.log('Table created');
 
-    console.log('Tables in the database:');
-    results.forEach((result) => {
-      console.log(result[fields[0].name]);
-    });
-
-    // Close the connection after querying (if you want)
-    connection.end((err) => {
+    // Show tables
+    connection.query('SHOW TABLES', (err, results, fields) => {
       if (err) {
-        console.error('Error closing connection:', err);
+        console.error('Error querying for tables:', err);
         return;
       }
-      console.log('Connection closed');
+
+      console.log('Tables in the database:');
+      results.forEach((result) => {
+        console.log(result[fields[0].name]);
+      });
+
+      // Close the connection after querying (if you want)
+      connection.end((err) => {
+        if (err) {
+          console.error('Error closing connection:', err);
+          return;
+        }
+        console.log('Connection closed');
+      });
     });
   });
 });
